bb0469b0ebe5701621d024b73e66537e59b35be4
Danylena <eleorex@yandex.ru>
2023-08-31 18:09:52 +0300

    04 - add task solution - Remove Duplicates

---
+++ problems/RemoveDuplicates.java
@@ -0,0 +1,28 @@
+package problems;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+
+        int uniqueValue = removeDuplicates(nums);
+
+        for (int i = 0; i < uniqueValue ; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    public static int  removeDuplicates(int[] nums) {
+
+        int k = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i-1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}

7168af7055bb838490a09a6a2ac26d6a199596be
mkhamkha <eleorex@yandex.ru>
2024-11-05 19:02:07 +0300

    код стайл

--- problems/TwoSum3.java
+++ problems/TwoSum3.java
@@ -1,50 +1,4 @@
 package problems;
-//
-//import java.util.HashSet;
-//import java.util.Scanner;
-//
-//public class TwoSum3 {
-//
-//    public static void main(String[] args) {
-//
-//        Scanner scanner = new Scanner(System.in);
-//
-//        // Читаем количество фишек
-//        int n = scanner.nextInt();
-//        int[] tokens = new int[n];
-//
-//        // Читаем очки на фишках
-//        for (int i = 0; i < n; i++) {
-//            tokens[i] = scanner.nextInt();
-//        }
-//
-//        // Читаем загаданное число k
-//        int k = scanner.nextInt();
-//
-//        // Создаем множество для хранения уже просмотренных значений
-//        HashSet<Integer> previous = new HashSet<>();
-//
-//        // Ищем пару, сумма которой равна k
-//        for (int A : tokens) {
-//            int Y = k - A; // Вычисляем необходимое значение
-//
-//            // Если Y уже есть в множестве, значит, нашли пару
-//            if (previous.contains(Y)) {
-//                System.out.println(A + " " + Y);
-//                scanner.close();
-//                return; // Завершаем выполнение после нахождения первой пары
-//            }
-//
-//            // Добавляем текущее значение A в множество
-//            previous.add(A);
-//        }
-//
-//        // Если подходящей пары не найдена
-//        System.out.println("None");
-//        scanner.close();
-//    }
-//}
-
 
 import java.util.HashSet;
 import java.util.Scanner;
@@ -55,31 +9,38 @@ public class TwoSum3 {
 
         Scanner scanner = new Scanner(System.in);
 
+        // Читаем количество фишек
         int n = scanner.nextInt();
         int[] tokens = new int[n];
 
+        // Читаем очки на фишках
         for (int i = 0; i < n; i++) {
             tokens[i] = scanner.nextInt();
         }
 
+        // Читаем загаданное число k
         int k = scanner.nextInt();
 
+        // Создаем множество для хранения уже просмотренных значений
         HashSet<Integer> previous = new HashSet<>();
 
+        // Ищем пару, сумма которой равна k
         for (int A : tokens) {
-            int Y = k - A;
+            int Y = k - A; // Вычисляем необходимое значение
 
+            // Если Y уже есть в множестве, значит, нашли пару
             if (previous.contains(Y)) {
                 System.out.println(A + " " + Y);
                 scanner.close();
-                return;
+                return; // Завершаем выполнение после нахождения первой пары
             }
 
+            // Добавляем текущее значение A в множество
             previous.add(A);
         }
 
+        // Если подходящей пары не найдена
         System.out.println("None");
         scanner.close();
     }
 }
-
